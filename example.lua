55344a08bd0796fcb9f4bf9946c140d85d1f7cdf
jhein1_mac <superjamrs323@gmail.com>
2021-08-11 15:33:13 -0700

    Updated example to reflect removal of utf8 ignoring

--- example.lua
+++ example.lua
@@ -16,12 +16,3 @@ print(Serializer:tableToVars{
     local exampleNumber = math.huge
     local exampleString = "Hello! This is an example string containing '\n' and '\0', and even '\240\159\152\130'!"
 ]]
-
--- This is used to configure settings, currently there is only one setting: NoFormatUnknowns, which disables auto-
--- formatting of unknown chars (generally utf8)
-Serializer:configureSetting("NoFormatUnknowns", true)
-
-print(Serializer:valueToVar("This is an unformatted utf8 char: 😂", "unformatted"))
---[[OUTPUT:
-    local unformatted = "This is an unformatted utf8 char: 😂"
-]]
